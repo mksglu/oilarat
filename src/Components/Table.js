7d1b4fab0250366e6f24eecd02f2ecae03c4f0f8
Mert Köseoğlu <info@mkoseoglu.com>
2017-12-27 12:24:16 +0300

    Inıtıal

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import pickBy from 'lodash/pickBy';
-import keys from 'lodash/keys';
-import mapValues from 'lodash/mapValues';
+import pickBy from "lodash/pickBy";
+import keys from "lodash/keys";
+import mapValues from "lodash/mapValues";
 
 import Row from "./Row";
 import Checkbox from "./Checkbox";
@@ -12,58 +12,47 @@ class Table extends Component {
     super(props);
 
     this.state = {
-      checkBoxControl : {},
-      isCheckedAll : false
+      checkBoxControl: {},
+      isCheckedAll: false
     };
 
     this.handleSelect = this.handleSelect.bind(this);
   }
 
   handleSelect(e, checkAll) {
-    let currentState = this.state.checkBoxControl
-    if(checkAll){
+    let currentState = this.state.checkBoxControl;
+    if (checkAll) {
       currentState = mapValues(currentState, () => !this.state.isCheckedAll);
       this.setState({
         isCheckedAll: !this.state.isCheckedAll
-      })
-    }else{
+      });
+    } else {
       const uid = e.target.value;
-      if(!this.state.isCheckedAll){
-        currentState[uid] = !currentState[uid]
+      if (!this.state.isCheckedAll) {
+        currentState[uid] = !currentState[uid];
       }
     }
 
     this.setState({
       checkBoxControl: currentState
-    })
-    this.props.selectedUser(keys(pickBy(currentState)))
+    });
+    this.props.selectedUser(keys(pickBy(currentState)));
   }
 
   componentWillReceiveProps(props) {
-    if(!props.userList.data.length){
-      return false
+    if (!props.userList.data.length) {
+      return false;
     }
-    const data = props.userList.data
-    const checkBoxControl = {}
-    //DATA[KEY] = 1. user data [key].İd = 1.user ın İd sİ 
-    // Ben böyle bir işlem yaptıgımda donduden cıkamıyorum. Tum ıslemlerımı dongude bıtırmek zorunda kalıyorum.
-    // Sen burada donguden nasıl cıktın O NEDEMEK NEDEN CAPS KALDI BENDE YAasdasdasd heh
-    // Ben sanıyordum ki dongude olusturduugm seyi dongu dısında erısemem sen bunnn ıcını
-    // checkBoxControl dongu ıcerısınde olusturuyosun bro ama state kısmın
-    // abi döngü out of scope birşey değilki kendi içerisinde ayrı bi function açmıyo yani aynı scope da
-    // anda cok iyi dimi neey genel :D böyle her seye erısıp acıp kapatıyosun felan 
-    // ne guzel seyler ya sdfg lodash ı nerden calısabılırım anda ben boyle lan bunu nasıl yaparım dedıgımde
-    // aklıma lodash gelmeli 
-    // bende yeni sayılırım lodash de pek öyle ahım şahım bilgim yok ama 
-    // genelde stack overflow da bulduğum cevaplar
-    // su dun attıgım todo seyıne baktın mı adam lodahs kullanmıs eet guzeldı
-    for(var key in data){
-      checkBoxControl[data[key].id] = false
+    const data = props.userList.data;
+    const checkBoxControl = {};
+
+    for (var key in data) {
+      checkBoxControl[data[key].id] = false;
     }
 
     this.setState({
-      checkBoxControl : checkBoxControl
-    })
+      checkBoxControl: checkBoxControl
+    });
   }
 
   render() {
@@ -76,7 +65,7 @@ class Table extends Component {
                 <tr>
                   <th>
                     <Checkbox
-                      onChange={(e) => this.handleSelect(e, true)} 
+                      onChange={e => this.handleSelect(e, true)}
                       checked={this.state.isCheckedAll}
                     />
                   </th>
